4df000da6de2413553d3456cbe346e9f8e88f5e9
afonyaAR <afonya290346@gmail.com>
2023-04-27 21:20:41 +0300

    Add existing file

---
+++ TSD/lab8/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class BinaryTree<T>
+    {
+        private Node<T> head;
+        public BinaryTree(T value)
+        {
+            
+        }
+
+        public void Add(T value)
+        {
+            if (head == null) 
+            {  
+                head = new Node<TNode>(value);
+            }
+            else
+            {
+                if (value > head.Value)
+                {
+
+                }
+            }
+        }
+        class Node<TNode> : IComparable<TNode>
+        where TNode : IComparable<TNode>
+        {
+            public TNode Value { get; private set; }
+            public TNode Left { get; set; }
+            public TNode Right { get; set; }
+            public Node(TNode value) { Value = value; }
+
+            public int CompareTo(TNode other)
+            {
+                return Value.CompareTo(other);
+            }
+        }
+    }
+}
