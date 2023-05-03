eb9ae88ce7d0034ffe36aa7e1406f359ab86c3f6
afonya <azarenok20031975@gmail.com>
2023-05-03 17:37:59 +0300

    -

--- OOP/lab3/Time.h
+++ OOP/lab3/Time.h
@@ -1,6 +1,38 @@
 #pragma once
+#include <iostream>
+using namespace std;
+
 class Time
 {
+private:
+	int hours;
+	int minutes;
+	int seconds;
+public:
+	Time(int hours = 0,
+		int minutes = 0,
+		int seconds = 0) 
+	{
+		int minsToAdd = 0;
+		int hrsToAdd = 0;
+		if (seconds < 0 || seconds > 59)
+		{
+			minsToAdd = seconds / 60;
+			this->seconds = seconds % 60;
+		}
+		if (hours < 0)
+		{
+			this->hours = 0;
+		}
+		else
+		{
+			this->hours = hours;
+		}
 
+		if (minutes < 0 || minutes > 59)
+		{
+			this->minutes = 
+		}
+	}
 };
 
