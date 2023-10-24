faf88c8f2f4b059d157e023ef46185880ff7ecad
afonya <azarenok20031975@gmail.com>
2023-10-24 11:29:23 +0300

    lab11, lab12

---
+++ OOP/sem2/lab11/lab11.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    ifstream ifs;
+
+    ifs.open("textfile.txt", ios::in);
+
+    if (!ifs)
+    {
+        cerr << "textfile.txt cant be opened for reading";
+        exit(1);
+    }
+
+    string* sentences = new string[3];
+    for (int i = 0; i < 3; i++)
+    {
+        getline(ifs, sentences[i]);
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        cout << sentences[2 - i] << endl;
+    }
+
+    ifs.close();
+}
+
