51855ad5dcb02b4e8b7365cc84390fc1a3cc98c8
afonya <azarenok20031975@gmail.com>
2024-01-21 15:48:22 +0300

    21.01

---
+++ OOP/code simplicity/lab7/lab7.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream> 
+using namespace std;
+
+void bubbleSort(int arr[], int size)
+{
+    for (int i = 0; i < size - 1; i++)
+    {
+        bool isSorted = true;
+        for (int j = 0; j < size - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+                isSorted = false;
+            }
+        }
+        if (isSorted)
+        {
+            break;
+        }
+    }
+}
+
+int main()
+{
+    int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    bubbleSort(arr, n);
+
+    cout << "Sorted array: \n";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
